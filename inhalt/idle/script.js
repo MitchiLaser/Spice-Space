d0a8e2023b7dcac8233fe5ac106c1d960b8905ec
Michael Hohenstein <michael.hohenstein@gmx.de>
2020-04-11 15:46:12 +0200

    added fake content, waiting for the easter egg

--- inhalt/idle/script.js
+++ inhalt/idle/script.js
@@ -1,6 +1,21 @@
 window.addEventListener("load", function(){
     window.setTimeout(function(){
         document.getElementsByTagName("main")[0].children[0].style.display = "none";
-        document.getElementsByTagName("main")[0].children[1].style.display = "flex";
+
+        window.addEventListener('online',  updateStatus);
+        window.addEventListener('offline', updateStatus);
+
+        updateStatus();
     }, 3000);
 });
+
+function updateStatus(event){
+    if(navigator.onLine){
+        document.getElementsByTagName("main")[0].children[1].style.display = "flex";
+        document.getElementsByTagName("main")[0].children[2].style.display = "none";
+    }
+    else{
+        document.getElementsByTagName("main")[0].children[1].style.display = "none";
+        document.getElementsByTagName("main")[0].children[2].style.display = "flex"; // fake-content
+    }
+}
