4470eeeb1fe092a82f4fb0b34c5bcc3319c03152
Michael Hohenstein <michael.hohenstein@gmx.de>
2020-04-12 16:57:55 +0200

    don't know what I changed before I took a nap but it seems to be important

--- inhalt/idle/script.js
+++ inhalt/idle/script.js
@@ -1,3 +1,14 @@
+function updateStatus(event){
+    if(navigator.onLine){
+        document.getElementsByTagName("main")[0].children[1].style.display = "flex";
+        document.getElementsByTagName("main")[0].children[2].style.display = "none";
+    }
+    else{
+        document.getElementsByTagName("main")[0].children[1].style.display = "none";
+        document.getElementsByTagName("main")[0].children[2].style.display = "block";
+    }
+}
+
 window.addEventListener("load", function(){
     window.setTimeout(function(){
         document.getElementsByTagName("main")[0].children[0].style.display = "none";
@@ -10,14 +21,3 @@ window.addEventListener("load", function(){
         window.setInterval(updateStatus, 1000);
     }, 3000);
 });
-
-function updateStatus(event){
-    if(navigator.onLine){
-        document.getElementsByTagName("main")[0].children[1].style.display = "flex";
-        document.getElementsByTagName("main")[0].children[2].style.display = "none";
-    }
-    else{
-        document.getElementsByTagName("main")[0].children[1].style.display = "none";
-        document.getElementsByTagName("main")[0].children[2].style.display = "block";
-    }
-}
