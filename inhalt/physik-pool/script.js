d03007c3a9949969b811e64909c9621f2c34299d
Michael Hohenstein <michael.hohenstein@gmx.de>
2020-04-20 17:16:45 +0200

    pool: fancy scroll animation

--- inhalt/physik-pool/script.js
+++ inhalt/physik-pool/script.js
@@ -1,3 +1,19 @@
 window.addEventListener("load", function(){
-    // scroll-Effekt initialisieren
+    var nav_elements = document.getElementsByTagName("nav")[0].getElementsByTagName("li");
+
+    for(var i = 0; i < nav_elements.length; i++){
+        var link = nav_elements[i].firstChild.getAttribute("href");
+
+        nav_elements[i].addEventListener("click", ClicKHandler(link));
+
+        nav_elements[i].firstChild.removeAttribute("href");
+    }
 });
+
+function ClicKHandler(e) {
+    return function () { callback(e) };
+}
+
+function callback(e) {
+    document.getElementById(e.substr(1)).scrollIntoView({behavior: "smooth"});
+}
